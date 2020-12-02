4c9684e7e2021e4888a8c0d94ab446957ce98ee5
hi20160616 <hi20160616@163.com>
2020-12-02 12:56:56 +0800

    humanized interaction update

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main():
     # c = Controller()
     #  c = Controller(src="./example", cfg="./example/config.txt")
     # c = Controller(cfg="./example/config.txt")
-    src = input("[+] Input tiff files dir(default is \".\"):")
-    cfg = input("[+] Input config file path(default is \"./config.txt\"):")
+    src = input("[+] Input tiff files dir(default is \".\"): ")
+    cfg = input("[+] Input config file path(default is \"./config.txt\"): ")
     c = Controller(src, cfg)
     c.read_cfg(c.cfg)
     c.scan_tifs(c.src)
@@ -20,8 +20,15 @@ if __name__ == "__main__":
     while 1:
         try:
             main()
-            input("[+] Done. Press any key to exit.")
-            sys.exit(0)
+            what = input("[+] Done.\n\n\n" +
+                         "[!] Press Enter to continue\n" +
+                         "[!] OR\n" +
+                         "[!] Input `exit` to quit job.\n" +
+                         "[*] >>> ")
+            if what == "exit":
+                sys.exit(0)
+            else:
+                continue
         except FileNotFoundError:
             print("[-] Please make sure config file at right spot as you input.")
         except Exception as err:
